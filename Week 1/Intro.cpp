c38af07c05f13ca239ecbb6b9890cdf1837ccebb
Arif <nurarifns18@gmail.com>
2021-10-16 23:17:34 +0800

    Create Intro.cpp

---
+++ Week 1/Intro.cpp
@@ -0,0 +1,16 @@
+//header file
+#include <iostream>
+
+//namespace: to use cin, cout, and endl etc from the header file included.
+using namespace std;
+
+//the main block to run code 
+int main(){
+
+	//display output
+	cout << "Intro to code";
+	
+	//code run as expected and will return the value 0
+	return 0;
+}
+
